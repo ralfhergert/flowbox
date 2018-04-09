6140b8108d79267a42152df3e8ec49a8500873fb
Ralf Hergert <ralf.hergert@gmx.de>
2018-04-09 09:43:43 +0200

    added JavaDoc comment

--- src/main/java/de/ralfhergert/math/geom/VertexLocation.java
+++ src/main/java/de/ralfhergert/math/geom/VertexLocation.java
@@ -1,5 +1,9 @@
 package de.ralfhergert.math.geom;
 
+/**
+ * This enum covers all three possible results for
+ * {@link Mesh#calcVertexLocation(Vertex)}.
+ */
 public enum VertexLocation {
 	InBounds,
 	OnBounds,
